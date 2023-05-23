e0584608ef24f64c9418a3cc85cb97fc959b5f6d
rayin19 <shrey163@gmail.com>
2023-05-23 19:33:40 +0530

    Default LibGDX Project

---
+++ android/src/com/rayin19/v2/pokemon/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.rayin19.v2.pokemon;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.rayin19.v2.pokemon.Main;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Main(), config);
+	}
+}
---
+++ desktop/src/com/rayin19/v2/pokemon/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.rayin19.v2.pokemon;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.rayin19.v2.pokemon.Main;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Pokemon Game");
+		new Lwjgl3Application(new Main(), config);
+	}
+}
